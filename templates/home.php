2b1957e41d40b25bed396a3fce9652a06a2c5669
Zeller <matheo.zeller@gmail.com>
2020-09-01 16:23:00 +0200

    feat(MVC): create MVC structure & namespace

---
+++ templates/home.php
@@ -0,0 +1,20 @@
+<?php
+require '../src/DAO/DAO.php';
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Mon blog</title>
+</head>
+
+<body>
+  <div>
+    <h1>App Retour</h1>
+    <p>En construction</p>
+  </div>
+</body>
+
+</html>
