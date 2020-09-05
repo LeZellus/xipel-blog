0f4c418994f261b23a78badd7dbb4e80c73d825b
Zelller <matheo.zeller@gmail.com>
2020-09-05 20:15:12 +0200

    feat(user): add feature to login

--- templates/home.php
+++ templates/home.php
@@ -1,7 +1,9 @@
 <?php $this->title = "Accueil"; ?>
 <?= $this->session->show('register'); ?>
+<?= $this->session->show('login'); ?>
 
 <h1>App Retour</h1>
 <p>En construction</p>
 
+<a href="../public/index.php?route=login">Connexion</a>
 <a href="../public/index.php?route=register">Inscription</a>
