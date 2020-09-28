86911441d4ab477ea6a0eac1ddf44cb7b70e49af
Zeller <matheo.zeller@gmail.com>
2020-09-28 11:26:49 +0200

    refactor(form): manage form easily

--- templates/edit_article.php
+++ templates/edit_article.php
@@ -2,20 +2,6 @@
 <h1>Mon blog</h1>
 <p>En construction</p>
 <div>
-  <form method="post" action="../public/index.php?route=editArticle&articleId=<?= htmlspecialchars($article->getId()); ?>">
-    <label for="title">Titre</label>
-    <input type="text" id="title" name="title" value="<?= htmlspecialchars($article->getTitle()); ?>">
-
-    <label for="content">Contenu</label>
-    <textarea id="content" name="content"><?= htmlspecialchars($article->getContent()); ?></textarea>
-
-    <label for="author">Auteur</label>
-    <input type="text" id="author" name="author" value="<?= htmlspecialchars($article->getAuthor()); ?>">
-
-    <label for="chapo">Chapo</label>
-    <input type="text" id="chapo" name="chapo" value="<?= htmlspecialchars($article->getChapo()); ?>">
-
-    <input type="submit" value="Mettre à jour" id="submit" name="submit">
-  </form>
+  <?php include('form_article.php'); ?>
   <a href="../public/index.php">Retour à l'accueil</a>
 </div>
