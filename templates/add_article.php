891739970199d874472a4a79e34dd05c37587b58
Zeller <matheo.zeller@gmail.com>
2020-09-28 15:01:56 +0200

    fix(error): codacy

--- templates/add_article.php
+++ templates/add_article.php
@@ -1,6 +1,6 @@
 <?php $this->title = "Nouvel article"; ?>
 <h1>Ajouter un article</h1>
 <div>
-    <?php include('form_article.php'); ?>
+    <?php include 'form_article.php'; ?>
     <a href="../public/index.php">Retour à l'accueil</a>
 </div>
